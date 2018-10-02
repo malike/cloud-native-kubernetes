0bd2bf8677c9d7873df0fe119939cb4918c9cb80
malike <st.malike@gmail.com>
2018-10-02 21:09:11 +0000

    fiexed test errors in java service

--- .travis/pushimage.sh
+++ .travis/pushimage.sh
@@ -6,8 +6,8 @@ echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USER" --password-stdin
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 export IMAGE_NAME=stmalike/$SERVICE_DIR
 docker build -f Dockerfile -t $IMAGE_NAME:$TAG .
-# docker tag $IMAGE_NAME:$COMMIT $IMAGE_NAME:$TAG
-# docker tag $IMAGE_NAME:$COMMIT $IMAGE_NAME:travis-$TRAVIS_BUILD_NUMBER
+docker tag $IMAGE_NAME:$COMMIT $IMAGE_NAME:$TAG
+docker tag $IMAGE_NAME:$COMMIT $IMAGE_NAME:travis-$TRAVIS_BUILD_NUMBER
 docker push $IMAGE_NAME
 
   
