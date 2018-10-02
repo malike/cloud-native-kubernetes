a3f20e530126adaa8d9c32ce0e2371b847dc48ea
malike <st.malike@gmail.com>
2018-10-02 21:33:09 +0000

    updated travis config for errors encountered when building the go service

--- .travis/pushimage.sh
+++ .travis/pushimage.sh
@@ -3,6 +3,8 @@
 SERVICE_DIR=$1
 echo "Uploading for image "+$SERVICE_DIR
 echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USER" --password-stdin
+cd $SERVICE_DIR
+echo "Switching directory " `pwd` 
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 export IMAGE_NAME=stmalike/$SERVICE_DIR
 docker build -f Dockerfile -t $IMAGE_NAME:$TAG .
