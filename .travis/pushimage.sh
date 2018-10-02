62d588ee204d2e5fd27098a750dac1ab660f0f84
malike <st.malike@gmail.com>
2018-10-02 19:30:32 +0000

    travis config to build docker images automacatically

--- .travis/pushimage.sh
+++ .travis/pushimage.sh
@@ -3,7 +3,6 @@
 SERVICE_DIR=$1
 echo "Uploading for image "+$SERVICE_DIR
 echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USER" --password-stdin
-cd $SERVICE_DIR
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 export IMAGE_NAME=stmalike/$SERVICE_DIR
 docker build -f Dockerfile -t $IMAGE_NAME:$TAG .
