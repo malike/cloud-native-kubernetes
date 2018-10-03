a091ba6f30467756507d114d23be36862f892d0d
malike <st.malike@gmail.com>
2018-10-03 07:12:45 +0000

    updated go dockerfile

--- .travis/pushimage.sh
+++ .travis/pushimage.sh
@@ -3,7 +3,7 @@
 SERVICE_DIR=$1
 echo "Uploading for image "+$SERVICE_DIR
 echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USER" --password-stdin
-cd $SERVICE_DIR
+# cd $SERVICE_DIR
 echo "Switching directory " `pwd` 
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 export IMAGE_NAME=stmalike/$SERVICE_DIR
