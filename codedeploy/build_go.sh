ef58da4d4f0d6bbaad412ca357fd7ed40023f671
malike <st.malike@gmail.com>
2019-01-20 23:14:07 +0000

    changes to pipeline and codedeploy files, added buildspec file for codedeploy

--- codedeploy/build_go.sh
+++ codedeploy/build_go.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+echo "Building GO IMAGE "
+cd ../frontend/
+echo "Switching directory " `pwd` 
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+export IMAGE_NAME=stmalike/$SERVICE_DIR
+docker build -f Dockerfile -t $IMAGE_NAME:$TAG .
+docker tag $IMAGE_NAME:$COMMIT $IMAGE_NAME:$TAG
+docker tag $IMAGE_NAME:$COMMIT $IMAGE_NAME:travis-$TRAVIS_BUILD_NUMBER
--- codedeploy/build_java.sh
+++ codedeploy/build_java.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+echo "Building JAVA IMAGE "
+cd ../java-service/
+echo "Switching directory " `pwd` 
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+export IMAGE_NAME=stmalike/$SERVICE_DIR
+docker build -f Dockerfile -t $IMAGE_NAME:$TAG .
+docker tag $IMAGE_NAME:$COMMIT $IMAGE_NAME:$TAG
+docker tag $IMAGE_NAME:$COMMIT $IMAGE_NAME:travis-$TRAVIS_BUILD_NUMBER
